a1fa471e6f72fb50349cdc54328b4f7d360664d9
rafa1dev <rafaellima61app@gmail.com>
2025-04-22 17:10:51 -0300

    fix: coloquei a coluna de email como unique

--- src/main/java/com/devrafa/NinjasCadastro/Missoes/MissoesModel.java
+++ src/main/java/com/devrafa/NinjasCadastro/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package com.devrafa.NinjasCadastro.Missoes;
 
 import com.devrafa.NinjasCadastro.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MissoesModel {
 
     @Id
@@ -20,4 +26,6 @@ public class MissoesModel {
     // uma missao pode ter varios ninjas, porem um ninja so pode ter uma missao!
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
+
+
 }
--- src/main/java/com/devrafa/NinjasCadastro/Ninjas/NinjaModel.java
+++ src/main/java/com/devrafa/NinjasCadastro/Ninjas/NinjaModel.java
@@ -2,11 +2,15 @@ package com.devrafa.NinjasCadastro.Ninjas;
 
 import com.devrafa.NinjasCadastro.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -14,45 +18,14 @@ public class NinjaModel {
     private Long id;
 
     private String nome;
+
+    @Column(unique = true)
     private String email;
+
     private int idade;
 
     // @ManyToOne, um ninja tem uma unica missao.
     @ManyToOne
     @JoinColumn(name = "missoes_id") // foring key ou chave estrangeira
     private MissoesModel missoes;
-
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
